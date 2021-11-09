b832d83f44e63cc158ff33571a46c66314da9959
noah solomon <noahtorah@gmail.com>
2021-11-09 15:14:10 -0500

    dim background

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const formButton = document.querySelector('.newsletter--button--open');
 const popup = document.querySelector('.newsletter--container');
 const close = document.querySelector('.newsletter--close');
+const body = document.querySelector('body')
 
 formButton.addEventListener('click', function(){
     popup.style.display = 'flex';
+    body.style.background = 'rgba(0, 0, 0, 0.5)';
    });
+
+   close.addEventListener('click', function() {
+    popup.style.display = 'none'
+})
+
+   
    
 
    
