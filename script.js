9b123ffd1eac6820c2b713c79d4d43ff2579d873
noah solomon <noahtorah@gmail.com>
2021-11-09 15:17:31 -0500

    preventdefault

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ const popup = document.querySelector('.newsletter--container');
 const close = document.querySelector('.newsletter--close');
 const body = document.querySelector('body')
 
-formButton.addEventListener('click', function(){
+formButton.addEventListener('click', function(e){
     popup.style.display = 'flex';
     body.style.background = 'rgba(0, 0, 0, 0.5)';
+    e.preventDefault();
    });
 
    close.addEventListener('click', function() {
