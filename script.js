a043b868b5976e92a483565269eec0360ea9426e
noah solomon <noahtorah@gmail.com>
2021-11-09 14:56:48 -0500

    newsletter work

---
+++ script.js
@@ -0,0 +1,10 @@
+const formButton = document.querySelector('.newsletter--button--open');
+const popup = document.querySelector('.newsletter--container');
+const close = document.querySelector('.newsletter--close');
+
+formButton.addEventListener('click', function(){
+    popup.style.display = 'flex';
+   });
+   
+
+   
